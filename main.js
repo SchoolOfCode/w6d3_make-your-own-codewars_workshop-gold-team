a6ca3a1dbab0202c737c0863b82018addcc4c472
sim89 <simrenkaursoor@gmail.com>
2021-12-22 16:38:17 +0000

    task 1 part a done

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 /* 
 👉 Write your kata here!
-*/
+
+// It's Christmas! You had to wait the whole year for this moment. You can already see all the presents under the Christmas tree. But you have to wait for the next morning in order to unwrap them. You really want to know, what's inside those boxes. But as a clever child, you can do your assumptions already.
+
+// You don't know whether you've been naughty or nice this year. So you may assume, that you'll only get things from your wishlist. You will see these presents, you can lift them and you can shake them a bit. Now you can make your assumptions about what you'll get(depending on if you were naughty or nice).
+
+you task will be to return an array of all children who will receive a gift. The child will receive a gift if:
+*they have been nice
+*left cookies for santa 
+*and is under 16 years of age
+
+const santasList = [
+    {childName: "littleTimmy", expectedGift: "iphone14", naughty: true or false, age: 35, leftACookie: true or false}
+    {childName: "joseph", expectedGift: "toy car", naughty: true or false, age: 5, leftACookie: true or false}
+    {childName: "berta", expectedGift: "doll house", naughty: true or false, age: 10, leftACookie: true or false}
+];
+
+
+
 
 //👉 Write the function your CodeWarriors will start with below here:
