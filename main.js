4f678b41a7cfe7f2cfc0c87284be8f642a3db625
Kagublanca <reecekagu@gmail.com>
2021-12-22 18:39:57 +0000

    Completed our own functionality... for testing!

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
-/* 
-👉 Write your kata here!
+// you task will be to return an array of all children who will receive a gift. The child will receive a gift if:
+// *they have been nice
+// *left cookies for santa
+// *and is under 16 years of age
 
-// It's Christmas! You had to wait the whole year for this moment. You can already see all the presents under the Christmas tree. But you have to wait for the next morning in order to unwrap them. You really want to know, what's inside those boxes. But as a clever child, you can do your assumptions already.
 
-// You don't know whether you've been naughty or nice this year. So you may assume, that you'll only get things from your wishlist. You will see these presents, you can lift them and you can shake them a bit. Now you can make your assumptions about what you'll get(depending on if you were naughty or nice).
 
-you task will be to return an array of all children who will receive a gift. The child will receive a gift if:
-*they have been nice
-*left cookies for santa 
-*and is under 16 years of age
+
+
+
 
 const santasList = [
-    {childName: "littleTimmy", expectedGift: "iphone14", naughty: true or false, age: 35, leftACookie: true or false}
-    {childName: "joseph", expectedGift: "toy car", naughty: true or false, age: 5, leftACookie: true or false}
-    {childName: "berta", expectedGift: "doll house", naughty: true or false, age: 10, leftACookie: true or false}
+    {childName: "Little Timmy", expectedGift: "iphone14", naughty: false, age: 35, leftACookie: true},
+    {childName: "Joseph", expectedGift: "toy car", naughty: false, age: 5, leftACookie: true},
+    {childName: "Berta", expectedGift: "doll house", naughty: true, age: 10, leftACookie: false},
+    {childName: "Reece", expectedGift: "doll house", naughty: false, age: 4, leftACookie: true},
+    {childName: "Simren", expectedGift: "doll house", naughty: true, age: 7, leftACookie: false},
+    {childName: "Troy", expectedGift: "doll house", naughty: true, age: 1, leftACookie: false},
+    {childName: "", expectedGift: "doll house", naughty: true, age: 7, leftACookie: false}
 ];
 
+function generateGoodList(list){
+const goodList = [];
+for(i=0; i< santasList.length; i++){
+    if(list[i].naughty === false && list[i].age < 16 && list[i].leftACookie === true){
+    goodList.push(list[i].childName);
+}
+}
+console.log(goodList);
+return goodList;
+}
 
-
-
-//👉 Write the function your CodeWarriors will start with below here:
+generateGoodList(santasList);
