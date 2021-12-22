4d3cb3ea6a8c9c61de44108d40275987fc61eeae
Kagublanca <reecekagu@gmail.com>
2021-12-22 19:40:13 +0000

    Set up the test!

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
-// you task will be to return an array of all children who will receive a gift. The child will receive a gift if:
-// *they have been nice
-// *left cookies for santa
-// *and is under 16 years of age
 
+/* Santa has left his Christmas planning 'til the last minute!!! You being his right-hand-elf, must quickly
+assemble the Nice list before tomorrow morning!
 
+ Your task will be to return an array of all children who will receive a gift. The child will receive a gift if:
+ *They have been nice AND
+ *Left cookies for santa AND
+ *Is under 16 years of age AND
 
+Santa is picky about how his list looks - have the format EXACTLY as follows:
+Name: x. Gift: y.
 
+Where x is the childs name and y is the expected gift.
+*/
 
-
-
-const santasList = [
-    {childName: "Little Timmy", expectedGift: "iphone14", naughty: false, age: 35, leftACookie: true},
-    {childName: "Joseph", expectedGift: "toy car", naughty: false, age: 5, leftACookie: true},
-    {childName: "Berta", expectedGift: "doll house", naughty: true, age: 10, leftACookie: false},
-    {childName: "Reece", expectedGift: "doll house", naughty: false, age: 4, leftACookie: true},
-    {childName: "Simren", expectedGift: "doll house", naughty: true, age: 7, leftACookie: false},
-    {childName: "Troy", expectedGift: "doll house", naughty: true, age: 1, leftACookie: false},
-    {childName: "", expectedGift: "doll house", naughty: true, age: 7, leftACookie: false}
+//Input
+export const santasList = [
+    {childName: "Little Timmy", expectedGift: "iPhone 14", naughty: false, age: 35, leftACookie: true},
+    {childName: "Joseph", expectedGift: "Toy car", naughty: false, age: 5, leftACookie: true},
+    {childName: "Berta", expectedGift: "Doll house", naughty: true, age: 10, leftACookie: false},
+    {childName: "Reece", expectedGift: "Hummus", naughty: false, age: 4, leftACookie: true},
+    {childName: "Simren", expectedGift: "Anotehr object", naughty: false, age: 7, leftACookie: true},
+    {childName: "Troy", expectedGift: "Something else...", naughty: false, age: 1, leftACookie: true},
+    {childName: "Nadja", expectedGift: "Potion of Youth", naughty: false, age: 1309, leftACookie: true},
+    {childName: "Cookie Monster", expectedGift: "Cookie", naughty: false, age: 10, leftACookie: false}
 ];
 
-function generateGoodList(list){
+
+//Function
+export function generateGoodList(list){
 const goodList = [];
-for(i=0; i< santasList.length; i++){
+for(let i=0; i< santasList.length; i++){
     let child = list[i]
     if(child.naughty === false && child.age < 16 && child.leftACookie === true){
-    goodList.push(list[i].childName);
+    goodList.push(`Name: ${child.childName}. Gift: ${child.expectedGift}.`);
 }
 }
 console.log(goodList);
 return goodList;
 }
 
-generateGoodList(santasList);
